3f7b52a27c0c492b8953c5f394bb2c92c488d572
Annamalai1995 <s.annamalai1995@gmail.com>
2024-08-05 11:39:37 +0530

    Variables

---
+++ src/main/java/Core_java/Variables/Demo_variables.java
@@ -0,0 +1,34 @@
+package Core_java.Variables;
+
+public class Demo_variables {
+     String Employeename2="Priya"; // Global variable
+
+    static  String Employee_name="Sathish";  //Static variables
+    public void Details() //Local Variable
+    {
+        String EmployeeName1="Annamalai";
+        System.out.println("My EmployeeName FIrst:"+EmployeeName1);
+        System.out.println("EmployeeName2 is "+Employeename2);
+    }
+
+
+    public static void main(String[] args) {
+
+        //Object Creation:
+        //syntax
+        //classname objectname= new classname();
+        Demo_variables dd= new Demo_variables();
+        //objectname.methodname
+        dd.Details();
+
+
+        String CompanyName="Zealous Tech Corp";   //Instance Variable
+        System.out.println("My companyName is:"+CompanyName);
+        System.out.println("MY Employee Name is:"+Employee_name);
+
+    }
+    static
+    {
+        System.out.println("Welcome to zealous Tech Corp");
+    }
+}
