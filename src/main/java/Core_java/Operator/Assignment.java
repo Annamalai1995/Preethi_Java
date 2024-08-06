0b9c1ee0f753eabea4f4e105857c1b6483cd2e07
Annamalai1995 <s.annamalai1995@gmail.com>
2024-08-06 12:53:01 +0530

    Operators

---
+++ src/main/java/Core_java/Operator/Assignment.java
@@ -0,0 +1,22 @@
+package Core_java.Operator;
+
+public class Assignment {
+    public static void main(String[] args) {
+        //Assigment
+        //+=,-=,*=,=..;
+        int cloud_computing=80;
+        int python=95;
+        int java=82;
+        int computer_network=95;
+        int cryptography=75;
+        cloud_computing+=10;
+        System.out.println("CloudCOmputing:"+cloud_computing);
+        python-=15;
+        System.out.println("Python :"+python);
+        cryptography*=10;
+        System.out.println("Cryptography"+cryptography);
+        java/=2;
+        System.out.println(java+"Java");
+
+    }
+}
