69bb0ed32a0f0b21e64be79243c2934fbd60c3cf
Annamalai1995 <s.annamalai1995@gmail.com>
2024-08-06 12:56:00 +0530

    Operators

---
+++ src/main/java/Core_java/Operator/IncreandDecre.java
@@ -0,0 +1,15 @@
+package Core_java.Operator;
+
+public class IncreandDecre {
+    public static void main(String[] args) {
+        //Pre increment
+        int cosmo=200;
+        int alpha=199;
+        System.out.println(++cosmo);
+        System.out.println(--alpha);
+        //Post Increment
+        System.out.println(cosmo++);
+        System.out.println(alpha--);
+
+    }
+}
