50035ff9e551208a85dab111933ad232bb051566
Annamalai1995 <s.annamalai1995@gmail.com>
2024-08-07 12:45:18 +0530

    Typecating,methods,scannerclass

---
+++ src/main/java/Core_java/Methodstypes/Demo_Typemethods.java
@@ -0,0 +1,44 @@
+package Core_java.Methodstypes;
+
+import java.util.Scanner;
+
+public class Demo_Typemethods {
+    //with return type and without arguments
+    public void woargs()
+    {
+        int ofc_count=55;
+        int toppercount=15;
+        System.out.println("OFFICE COUNT "+ofc_count+""+"TOPPER COUNT"+toppercount);
+
+    }
+    //wo returntype wo args
+    public  String setname()
+    {
+        String name ="Annamalai";
+        return  name;
+    }
+    //with return type and with agruments
+    public void Passingmethod(float salary,long phnno)
+    {
+        double age=salary;
+        System.out.println("My salary is"+salary);
+        System.out.println("Age"+age);
+        System.out.println("PHN NO:"+phnno);
+
+
+    }
+
+
+
+    public static void main(String[] args) {
+        Demo_Typemethods dt=new Demo_Typemethods();
+        dt.woargs();
+       String getname=dt.setname();
+        System.out.println("Getting name :"+getname);
+        Scanner ss= new Scanner(System.in);
+        System.out.println("Enter contact number");
+        Long mblno=ss.nextLong();
+        dt.Passingmethod(4.5f,mblno);
+
+    }
+}
---
+++ src/main/java/Core_java/casting/Demo_casting.java
@@ -0,0 +1,22 @@
+package Core_java.casting;
+
+public class Demo_casting {
+    public static void main(String[] args) {
+        //Changing the one datatype to another datatype
+        char sweet='R';
+        int delicious=sweet;
+        System.out.println("Sweet is :"+delicious);
+        //Narrowing large to small
+        double weight=50.5;
+        int myweight=(int)weight;
+        System.out.println("My weight:"+myweight);
+        long mblno=9688193688l;
+        int covertlong=(int) mblno;
+        System.out.println("MOBILE  NUMBER:"+covertlong);
+        int number=11;
+        char value=(char)number;
+        System.out.println("Char value is :"+value);
+
+
+    }
+}
