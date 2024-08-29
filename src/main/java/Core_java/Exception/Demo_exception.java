c8ac544ff50dbd193679ebe5bf1d4df63653727a
Annamalai1995 <s.annamalai1995@gmail.com>
2024-08-29 11:44:41 +0530

    ExceptionHandling

---
+++ src/main/java/Core_java/Exception/Demo_exception.java
@@ -0,0 +1,10 @@
+package Core_java.Exception;
+
+public class Demo_exception {
+    public static void main(String[] args) {
+        int data=500;
+        int value=0;
+        int result=data/value;
+        System.out.println(result);
+    }
+}
