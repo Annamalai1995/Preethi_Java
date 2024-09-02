5a7d8d786a60cd758d2f060a9754a50a4d1bca52
Annamalai1995 <s.annamalai1995@gmail.com>
2024-09-02 12:49:48 +0530

    Throws

---
+++ src/main/java/Core_java/Exception/ThrowExample.java
@@ -0,0 +1,19 @@
+package Core_java.Exception;
+
+public class ThrowExample {
+    void bloodElgigible(int age)
+    {
+        if(age>=18)
+        {
+            throw new ArithmeticException("You are not eligible");
+        }
+        else {
+            System.out.println("eligible");
+        }
+    }
+
+    public static void main(String[] args) {
+        ThrowExample te= new ThrowExample();
+        te.bloodElgigible(22);
+    }
+}
